f66b1c56bc4aa06c02cdf3aa08c2bbf52c38734f
IliaBulavintsev <twintipper@mail.ru>
2015-10-04 19:53:51 +0300

    last

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
 $(function() {
 	console.log(' main.js ready');
+
+	$('.add_background').on('click', function(e) {
+		e.preventDefault();
+		console.log("Call modal popUp");
+		$('.popup_modal').bPopup();
+	});
 	
 });
