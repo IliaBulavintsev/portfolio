1f92519ae97e15af7b650361cf09ff52a86fc110
IliaBulavintsev <twintipper@mail.ru>
2015-10-05 04:35:36 +0300

    second

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,46 @@
 $(function() {
 	console.log(' main.js ready');
+	
+	PopUpHide();
+	
+	$('.close_wrapper').bind('click', function(event) {
+		PopUpHide();
+		/* Act on the event */
+	});
 
-	$('.add_background').on('click', function(e) {
-		e.preventDefault();
+	//$('.popup').on('click', function(event) {
+	//	PopUpHide();
+		/* Act on the event */
+	//});
+	
+	$('.add_background').bind('click', function(e) {
+		//e.preventDefault();
+		//$('#popup_modal').bPopup();
+		PopUpShow();
 		console.log("Call modal popUp");
-		$('.popup_modal').bPopup();
+	});
+
+	$('.nav_menu_link').on('click', function(event) {
+		$('.activate').removeClass('activate');
+		$(this).addClass('activate');
+		console.log("change!");
+		/* Act on the event */
+	});
+
+	$('.activate').addClass('disable_link');
+
+	$('.input_file[name]').on('change()', function(event) {
+		console.log($);
+		//event.preventDefault();
+		/* Act on the event */
 	});
 	
 });
+
+function PopUpHide(){
+	$(".popup").fadeOut("slow");
+	console.log("popUp hide");
+}
+function PopUpShow(){
+	$(".popup").fadeIn("slow");
+}
